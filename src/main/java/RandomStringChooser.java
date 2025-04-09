b481a82ebbb098ca93f6c9ac93d09c7e1639a8f0
JaTimur <JaTimur@s.sfusd.edu>
2025-04-09 14:27:51 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,7 +1,15 @@
 import java.util.*;
-public class RandomStringChooser
-{
-  /* to be implemented in part (a) */
+public class RandomStringChooser{
+  private String[] list;
+  private int used;
+  //?
+  public RandomStringChooser(String[] array){
+    list=array;
+    used=0;
+  }
+  public String getNext(){
+    if(used>=list.length){}
+  }
   
   //Heads up! 
   //You will get a very confusing error message until you have working code in part b as well
